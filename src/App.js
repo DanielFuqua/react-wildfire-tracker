4c43ab8237f0bafc5e5b102cff41218686e0ed0b
robdan89 <fuquardwork@gmail.com>
2020-12-02 16:20:38 -0600

    passed eventData into Map.js through props. Mapped through all of the data and created a location marker component for all of the events that matched the Wildfire category id in the nasa API. App now displays all of the wildfires with a location marker icon around the world.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { useState, useEffect } from 'react'
 import Map from './components/Map'
-console.log(process.env.REACT_APP_GOOGLE_API_KEY)
+import Loader from './components/Loader'
+
 function App() {
+  const [eventData, setEventData] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    //fetchEvents is an asynchronous function using async/await that fetches the events at the nasa api (all of the fire coordinates and any other similar data)
+    const fetchEvents = async () => {
+      //set loading to true while data is being fetched
+      setLoading(true)
+
+      const res = await fetch ('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events')
+      const { events } = await res.json()
+
+      setEventData(events)
+      setLoading(false)
+    }
+
+    fetchEvents()
+
+    
+  }, [])
+
   return (
     <div>
-     <Map/>
+     { !loading ? <Map eventData={eventData}/> : <Loader/>}
     </div>
   );
 }
